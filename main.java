b3d049c08cabb85f422bdfb0f3b9096c63808b70
Unknown <stens30983@loswego.k12.or.us>
2017-12-15 11:36:45 -0800

    begin

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class main {
-  public void static main(String[] args) {
-    
+  public static void main(String[] args) {
+    System.out.println("Major Tom: Hello player. I am here to tech you about grammar. I hope you are ready to learn. In order to make things less awkward please tell me your name.");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println(name + ": M' neam iz " + name + ".");
   }
 }
